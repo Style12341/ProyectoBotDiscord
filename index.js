f3d07a027292fbfb7a2643e5e7bfba6d9306e3e9
Style12341 <snkargentocod@gmail.com>
2020-08-04 22:48:19 -0300

    Primer Commit
    Archivos Basicos

---
+++ index.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js');
+const { prefix, token } = require('./config.json');
+const client = new Discord.Client();
+
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+client.on('message', message => {
+	if (message.content === `${prefix}mendoza`) {
+        // send back "Pong." to the channel the message was sent in
+        message.channel.send('es La gorda');
+    }
+        else if (message.content === `${prefix}server`) {
+            message.channel.send(`En ${message.guild.name} hay ${message.guild.memberCount} miembros.`);
+        }
+});
+client.login(token);
