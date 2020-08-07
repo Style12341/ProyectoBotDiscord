6df646aa6713d32aaa1fd13c8ac7999ced5209e0
Style12341 <snkargentocod@gmail.com>
2020-08-07 18:52:33 -0300

    Pausado hasta terminar tutorial

--- commands/timer.js
+++ commands/timer.js
@@ -1,16 +1,23 @@
 const Discord = require('discord.js'); // Libreria de Discord para JS
-const msToTime = duration => {
+function msToTime(duration) {
     let seconds = Math.floor((duration / 1000) % 60),
       minutes = Math.floor((duration / (1000 * 60)) % 60),
       hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
     hours = (hours < 10) ? "0" + hours : hours;
     minutes = (minutes < 10) ? "0" + minutes : minutes;
     seconds = (seconds < 10) ? "0" + seconds : seconds;
-    if (hours == "00" && minutes == "00") {
-        return (`${seconds}s`);
+    let display = "";
+    if (hours !== "00") {
+        display += hours + "h";
     }
-    return (`${hours}h ${minutes}m ${seconds}s`);
-  };
+    if (minutes !== "00") {
+        display += minutes + "m";
+    }
+    if (seconds !== "00") {
+        display += seconds + "s";
+    }
+    return (display);
+  }
 module.exports = {
 	name: 'timer',
     description: 'Permite poner un timer para X cantidad de minutos',
@@ -19,24 +26,27 @@ module.exports = {
 	execute(message, args) {
         const now = Date.now();
         const timestamps = new Discord.Collection();
-        timestamps.set(message.author.id, now);
         let timer = 0;
         if (args.indexOf("r") == -1) {
             for (const argumento of args) {
+                console.log("inside for before if");
+                console.log(args);
+                console.log(argumento);
                 if (argumento == "h") {
                     const posh = args.indexOf("h");
+                    console.log("posh");
                     const numHoras = args[posh - 1];
                     timer += numHoras * 3600000 ;
                     args.splice(posh, 1);
                 }
                 else if (argumento == "m") {
-                    const posm = args.indexof("m");
+                    const posm = args.indexOf("m");
                     const numMinutos = args[args.indexOf("m") - 1];
                     timer += numMinutos * 60000 ;
                     args.splice(posm, 1);
                 }
                 else if (argumento == "s") {
-                    const poss = args.indexof("s");
+                    const poss = args.indexOf("s");
                     const numSegundos = args[args.indexOf("s") - 1];
                     timer += numSegundos * 1000 ;
                     args.splice(poss, 1);
@@ -44,12 +54,18 @@ module.exports = {
                 }
             args.unshift("r");
         }
-        const timerFinal = timestamps.get(message.author.id) + timer;
-        if (now < timerFinal) {
-            const timerRestante = (timerFinal - now) / 1000 ;
-
+        console.log(timer);
+        console.log(now);
+        if (timestamps.has(message.author.id)) {
+            var timerFinal = timestamps.get(message.author.id) + timer;
+            const timerRestante = (timerFinal - now);
+            console.log(timerRestante);
             return message.reply(`Al timer aun le quedan ${msToTime(timerRestante)}`);
         }
-		message.channel.send('Pong.');
+        timestamps.set(message.author.id, now);
+         if (timerFinal == now || now > timerFinal) {
+            return message.reply("Se termino el tiempo");
+        }
+		message.channel.send('test');
 	},
 };
