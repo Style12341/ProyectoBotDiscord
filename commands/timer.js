bfbcfc2480d56bb3421670a85b756ebff31b0406
Style12341 <snkargentocod@gmail.com>
2020-08-07 14:35:06 -0300

    Second Commit

--- commands/timer.js
+++ commands/timer.js
@@ -1,4 +1,16 @@
 const Discord = require('discord.js'); // Libreria de Discord para JS
+const msToTime = duration => {
+    let seconds = Math.floor((duration / 1000) % 60),
+      minutes = Math.floor((duration / (1000 * 60)) % 60),
+      hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
+    hours = (hours < 10) ? "0" + hours : hours;
+    minutes = (minutes < 10) ? "0" + minutes : minutes;
+    seconds = (seconds < 10) ? "0" + seconds : seconds;
+    if (hours == "00" && minutes == "00") {
+        return (`${seconds}s`);
+    }
+    return (`${hours}h ${minutes}m ${seconds}s`);
+  };
 module.exports = {
 	name: 'timer',
     description: 'Permite poner un timer para X cantidad de minutos',
@@ -7,8 +19,9 @@ module.exports = {
 	execute(message, args) {
         const now = Date.now();
         const timestamps = new Discord.Collection();
+        timestamps.set(message.author.id, now);
         let timer = 0;
-        if (args.indexOf("r") == -1) { 
+        if (args.indexOf("r") == -1) {
             for (const argumento of args) {
                 if (argumento == "h") {
                     const posh = args.indexOf("h");
@@ -28,10 +41,14 @@ module.exports = {
                     timer += numSegundos * 1000 ;
                     args.splice(poss, 1);
                 }
-                else{
-                    args.unshift("r");
                 }
-            }
+            args.unshift("r");
+        }
+        const timerFinal = timestamps.get(message.author.id) + timer;
+        if (now < timerFinal) {
+            const timerRestante = (timerFinal - now) / 1000 ;
+
+            return message.reply(`Al timer aun le quedan ${msToTime(timerRestante)}`);
         }
 		message.channel.send('Pong.');
 	},
