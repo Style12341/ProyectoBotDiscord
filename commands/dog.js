27b91143e4e06991220888a65746e2bb577aa223
Style12341 <snkargentocod@gmail.com>
2020-08-27 03:27:44 -0300

    fix heroku 5

--- commands/dog.js
+++ commands/dog.js
@@ -3,7 +3,7 @@ module.exports = {
     description: 'Proporciona la foto de un perrito',
     async execute(message, args) {
         const fetch = require('node-fetch'); // Requiere la libreria node fetch para trabajar con la API
-        const { message } = await fetch(`https://dog.ceo/api/breeds/image/random`).then(response => response.json()); // Crea una constante list que le pide a la API de perros su respectivo archivo JSON y lo guarda
+        const { file } = await fetch(`https://dog.ceo/api/breeds/image/random`).then(response => response.json()); // Crea una constante list que le pide a la API de perros su respectivo archivo JSON y lo guarda
         console.log(file);
         // return message.channel.send(file);
     },
