744da7a9d5f87dc7ed2951998d987752be3455f2
Maximiliano Bernard <58054478+Style12341@users.noreply.github.com>
2020-09-01 00:17:22 -0300

    Extra APIs (#3)
    * API libreria
    
    * Installed KSoft
    
    * Test
    
    * Added meme cute random wikihow

---
+++ commands/random.js
@@ -0,0 +1,53 @@
+const { KSoftClient } = require('@ksoft/api');
+const { ksoftToken } = require('../token.json');
+const ksoft = new KSoftClient(ksoftToken);
+const { prefix } = require('../config.json');
+module.exports = {
+    name: 'random',
+    description: 'Devuelve una imagen aleatoria.',
+    usage: '<tag>',
+    cooldown: 5,
+    async execute(message, args) {
+        const tagEmbed = { // Crea un embed inicial para mostrar la ayuda.
+            color: 0xff9900,
+            author: {
+                name: 'Tags',
+                icon_url: 'https://i.imgur.com/NAMH0Db.jpg',
+            },
+            fields: [
+                {
+                    name: 'Lista de tags:',
+                    value: 'Some value here',
+                },
+            ],
+            footer: {
+                text: `Utilize ${prefix}random <tag>`,
+            },
+        };
+        if (!args.length) {
+            const tagList = [];
+            const tagArray = await ksoft.images.tags();
+            for (const tags of tagArray) {
+                if (tags.nsfw == false) {
+                    tagList.push(tags.name);
+                }
+            }
+            const tags = tagList.join('` `');
+            tagEmbed.fields[0].value = `\`${tags}\``;
+            return message.reply({ embed: tagEmbed });
+        }
+        if (args[0] == "bird") {
+            args[0] = "birb";
+        }
+        try {
+            const image = await ksoft.images.random(args[0], { nsfw: false });
+            message.channel.send(image.url);
+        }
+        catch (error) {
+            console.error("Hubo un error");
+            message.reactions.removeAll();
+            message.react('❌');
+            message.reply('Hubo un error al ejecutar ese comando');
+        }
+    },
+};
---
+++ commands/wikihow.js
@@ -0,0 +1,20 @@
+const { KSoftClient } = require('@ksoft/api');
+const { ksoftToken } = require('../token.json');
+const ksoft = new KSoftClient(ksoftToken);
+module.exports = {
+    name: 'wikihow',
+    description: 'Devuelve un articulo de wikihow',
+    cooldown: 5,
+    async execute(message) {
+        const wikiH = await ksoft.images.wikihow();
+        const wikihowEmbed = { // Crea un embed inicial para mostrar la ayuda.
+            color: 0xff9900,
+            title: wikiH.article.title,
+            url: wikiH.article.link,
+            image: {
+                url: wikiH.url,
+            },
+        };
+        message.channel.send({ embed : wikihowEmbed });
+    },
+};
